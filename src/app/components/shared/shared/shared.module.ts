29f81774810c397fea14d2eb8e6966594598de6d
cesarp <capr99@hotmail.com>
2024-08-11 00:43:10 -0400

    redireccion en registro y login

--- src/app/components/shared/shared/shared.module.ts
+++ src/app/components/shared/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule, IonInput } from '@ionic/angular';
 import { UserButtonComponent } from 'src/app/components/atoms/user-button/user-button.component';
 import { DeleteaccountComponent } from '../../deleteaccount/deleteaccount.component';
-import { MapDistanceComponent } from '../../map-distance/map-distance.component';
+// import { MapDistanceComponent } from '../../map-distance/map-distance.component';
 import { UpdateemailComponent } from 'src/app/components/updateemail/updateemail.component';
 import { UpdatenameComponent } from '../../updatename/updatename.component';
 import { UpdatepasswordComponent } from '../../updatepassword/updatepassword.component';
@@ -15,9 +15,7 @@ import { UpdatepasswordComponent } from '../../updatepassword/updatepassword.com
   declarations: [
     // IonInput,
     UserButtonComponent, 
-    // ChatModalComponent, 
     DeleteaccountComponent, // no se usa mas de una vez
-    MapDistanceComponent,
     UpdateemailComponent, // no se usa mas de una vez
     UpdatenameComponent, // no se usa mas de una vez
     UpdatepasswordComponent, // no se usa mas de una vez
@@ -26,9 +24,7 @@ import { UpdatepasswordComponent } from '../../updatepassword/updatepassword.com
   exports: [
     // IonInput,
     UserButtonComponent, 
-    // ChatModalComponent, 
     DeleteaccountComponent, // no se usa mas de una vez
-    MapDistanceComponent,
     UpdateemailComponent, // no se usa mas de una vez
     UpdatenameComponent, // no se usa mas de una vez
     UpdatepasswordComponent // no se usa mas de una vez
