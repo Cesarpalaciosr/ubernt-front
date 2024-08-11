3864aede42bfaeb8576c135c68d3ea634127ad7e
cesarp <capr99@hotmail.com>
2024-08-10 22:37:24 -0400

    comprobando info card user

--- src/app/pages/home/saved/saved.module.ts
+++ src/app/pages/home/saved/saved.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { InfoCardUserComponent  } from "../../../app/components/atoms/info-card-user/info-card-user.component";
+import { InfoCardUserComponent  } from "../../../components/atoms/info-card-user/info-card-user.component";
 import { SavedPageRoutingModule } from '../../../routes/saved-routing.module';
 import { SavedPage } from './saved.page';
 
