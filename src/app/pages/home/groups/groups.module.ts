1ee0305b1961e66fb0544c1cf9705ab86b7f5ec6
MaferRep <mariafernandaboscan@gmail.com>
2024-08-25 03:34:47 -0400

    chat conexiones peliculas (cambiar)

--- src/app/pages/home/groups/groups.module.ts
+++ src/app/pages/home/groups/groups.module.ts
@@ -1,25 +1,14 @@
-// src/app/groups/groups.module.ts
-
+import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { GroupsPageRoutingModule } from '../../../routes/groups-routing.module';
-import { ChatModalComponent } from '../../../components/organisms/chat-modal/chat-modal.component';
+import { FormsModule } from '@angular/forms';
 import { GroupsPage } from './groups.page';
+import { ChatModalComponent } from 'src/app/components/organisms/chat-modal/chat-modal.component';
+
+import { GroupsPageRoutingModule } from 'src/app/routes/groups-routing.module';
 
 @NgModule({
-  declarations: [
-    ChatModalComponent,
-    GroupsPage,  
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    IonicModule,
-    GroupsPageRoutingModule,
-  ],
-
+  imports: [IonicModule, CommonModule, FormsModule, GroupsPageRoutingModule],
+  declarations: [GroupsPage, ChatModalComponent],
 })
-export class GroupsModule {}
+export class GroupsPageModule {}
--- src/app/pages/home/groups/groups.page.ts
+++ src/app/pages/home/groups/groups.page.ts
@@ -1,17 +1,93 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { ChatModalComponent } from 'src/app/components/organisms/chat-modal/chat-modal.component';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-groups',
-  templateUrl: './groups.page.html',
-  styleUrls: ['./groups.page.scss'],
+  templateUrl: 'groups.page.html',
+  styleUrls: ['groups.page.scss'],
 })
 export class GroupsPage implements OnInit {
-  public groups!: string;
-  private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  user: string = '';
+  users: any = [];
+  groups: any = [];
 
-  ngOnInit() {
-    this.groups = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    public modalController: ModalController
+  ) {}
+
+  searchUser(event: any) {
+    this.users = [];
+    if (event.target.value !== '') {
+      this.http
+        .post('https://tmdb-for-a-angularmovile.onrender.com/search', {
+          query: `${event.target.value}`,
+        })
+        .subscribe((data: any) => {
+          data.forEach((user: any) => {
+            if (user.alias !== this.user) {
+              this.users.push(user);
+            }
+          });
+        });
+    }
+    console.log(this.users);
+  }
+
+  openChat(user: any) {
+    this.http
+      .post('https://tmdb-for-a-angularmovile.onrender.com/chat', {
+        roomId: this.user + user.alias,
+        participants: [this.user, user.alias],
+      })
+      .subscribe((data: any) => {
+        // console.log(data);
+        const modal = this.modalController
+          .create({
+            component: ChatModalComponent,
+            componentProps: {
+              user: this.user,
+              participants: [this.user, user.alias],
+              roomId: data.roomId,
+            },
+          })
+          .then((modal) => {
+            modal.present();
+          });
+      });
+    // modal.then(modal => modal.present());
+  }
+
+  openGroup(group: any) {
+    const modal = this.modalController
+      .create({
+        component: ChatModalComponent,
+        componentProps: {
+          user: this.user,
+          participants: [],
+          roomId: group.roomId,
+        },
+      })
+      .then((modal) => {
+        modal.present();
+      });
+  }
+
+  async ngOnInit() {
+    const data = await Preferences.get({ key: 'alias' });
+    this.user = data.value!;
+
+    this.http
+      .get('https://tmdb-for-a-angularmovile.onrender.com/chat/public')
+      .subscribe((data: any) => {
+        console.log(data);
+        this.groups = data;
+      });
   }
 }
