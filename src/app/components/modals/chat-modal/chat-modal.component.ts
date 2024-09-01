a13cea049b3e70fee642fb6cb6daa99af4aed1ea
radsylph <andresa@devtop.io>
2024-09-01 02:46:43 -0400

    feat:added chat functionality, added socket service

--- src/app/components/modals/chat-modal/chat-modal.component.ts
+++ src/app/components/modals/chat-modal/chat-modal.component.ts
@@ -1,19 +1,20 @@
-// src/app/groups/chat-modal/chat-modal.component.ts
-
-import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewChecked, OnDestroy } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { io } from 'socket.io-client';
 import { environment } from '../../../../environments/environment';
-
+import { ChatService } from 'src/app/services/socket.service';
+import { ChatInterface, MessageInterface } from 'src/app/interfaces/chat.interface';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-chat-modal',
   templateUrl: './chat-modal.component.html',
   styleUrls: ['./chat-modal.component.scss'],
 })
-export class ChatModalComponent implements OnInit, AfterViewChecked {
+
+export class ChatModalComponent implements OnInit, AfterViewChecked, OnDestroy {
   @Input() user!: any;
   @Input() participants!: string[];
   @Input() roomId!: string;
@@ -22,78 +23,97 @@ export class ChatModalComponent implements OnInit, AfterViewChecked {
   message: string = '';
   messages: any[] = [];
   public chatName: string = '';
-  // public user 
   private socket: any;
   private localURL = environment.localURL;
-  public userInfo: any;
+  // public userInfo: any;
+  public userInfo: {
+    _id: string;
+    username: string;
+    fullName: string
+  } = {
+      _id: '',
+      username: '',
+      fullName: ''
+    }
   public users: any = [];
-
-
 
   constructor(
     public modalController: ModalController,
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private chatService: ChatService
   ) {
     this.socket = io(`${this.localURL}`, {
-      autoConnect: false, 
+      autoConnect: false,
     });
-    this.socket.on('messageFromAnother' , (data:any)=>{
+    this.socket.on('messageFromAnother', (data: any) => {
       console.log(data);
       this.messages.push(data);
       console.log(this.messages);
-    })
-  }
-
-
-  async getChatMessages(idRoom: string, participants: string[]) {
-    const data = { idRoom, participants };
-    console.log(data);
-    try {
-      return new Promise((resolve, reject) => {
-        this.http
-          .post(`${this.localURL}/chat/getChatMessages/${idRoom}`, {
-            participants,
-          })
-          .subscribe(
-            (data: any) => {
-              console.log(data);
-              resolve(data);
-            },
-            (error) => {
-              reject(error);
-            }
-          );
-      });
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
+    });
   }
 
   public async getMessages() {
     try {
-     
+      this.socket.emit('getMessages', {
+        roomId: this.roomId,
+        user: this.user,
+      });
+
+      this.socket.on('message', (data: any) => {
+        console.log(data);
+      });
     } catch (error) {
       this.closeModal();
-      console.log(error)
+      console.log(error);
     }
   }
 
-  sendMessage() {
-    console.log('hola');
-    
-  }
-  
-  public async getUserInfo() : Promise<void>{
-    await this.http.post(`${this.localURL}/auth/finduser` , {_id:this.user}).subscribe(async (res:any)=>{
-      console.log(res);
-      this.userInfo = res;
+  public async sendMessage() {
+    if (this.message === '') {
+      return;
+    }
+    const message = {
+      message: this.message,
+      idRoom: this.user,
+    };
+
+    const messageToSave: any = {
+      message: this.message,
+      sender: this.userInfo.username,
+      idSender: this.userInfo._id,
+      sendTo: this.roomId,
+      idRoom: this.roomId,
+      idSentTo: this.roomId,
+    };
+
+    this.socket.emit('chatMessage', {
+      message: this.message,
+      sender: this.user.username,
+      idSender: this.user._id,
+      idRoom: this.roomId,
     });
-    
+    try {
+      this.chatService.saveChatMessages(messageToSave);
+      this.message = '';
+    } catch (error) {
+      console.log(error);
+    }
   }
-  closeModal() {
-    console.log("adios")
+
+  public async getUserInfo(): Promise<void> {
+    const username: any = await Preferences.get({ key: 'username' });
+    const fullName: any = await Preferences.get({ key: 'fullname' });
+    const _id: any = await Preferences.get({ key: 'id' });
+    this.userInfo = {
+      _id: _id.value,
+      username: username.value,
+      fullName: fullName.value
+    };
+  }
+
+  public closeModal() {
+    console.log("adios");
     this.socket.emit('leaveRoom', {
       roomId: this.roomId,
     });
@@ -101,11 +121,9 @@ export class ChatModalComponent implements OnInit, AfterViewChecked {
     this.modalController.dismiss();
   }
 
-  
-
-  async ngOnInit() {
+  public async ngOnInit() {
     await this.getUserInfo();
-    console.log(this.localURL)
+    console.log(this.localURL);
     console.log(`connecting to the chat: ${this.roomId} with the user: ${this.user}`);
     this.socket.connect();
     this.socket.emit('joinRoom', {
@@ -113,18 +131,25 @@ export class ChatModalComponent implements OnInit, AfterViewChecked {
       user: this.userInfo
     });
     try {
-      const getChatMessagesTest:any = await this.getChatMessages(this.roomId , this.userInfo);
-      const messages:any =  getChatMessagesTest.getChatMessages;
-      this.messages = messages;
+      const ChatMessages: any = await this.chatService.getChatMessages(this.roomId, [this.userInfo._id]);
+      const messages: MessageInterface[] = ChatMessages.chatMessages;
+      if (messages.length > 0) {
+        this.messages = messages;
+        return;
+      }
+
     } catch (error) {
       console.log(error);
     }
-
   }
 
-  ngAfterViewChecked() {
+  public ngAfterViewChecked() {
     this.chatScroll.nativeElement.scrollIntoView({
       behavior: 'smooth',
     });
+  }
+
+  public ngOnDestroy() {
+    this.closeModal();
   }
 }
---
+++ src/app/services/socket.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ReplaySubject } from 'rxjs';
+import { io } from 'socket.io-client';
+import { environment } from '../../environments/environment';
+
+
+interface ChatInterface {
+    idRoom: string;
+    messages: string[];
+    participants: string[];
+    type: string;
+}
+
+interface MessageInterface {
+    idRoom: string;
+    idSender: string;
+    idSentTo?: string;
+    message: string;
+    sendTo?: string;
+    sender: string;
+}
+
+@Injectable()
+export class ChatService {
+    private socket: any;
+    private messages = new ReplaySubject<MessageInterface[]>(1);
+    private localURL = environment.localURL;
+    constructor(private http: HttpClient) { }
+
+    async getChatMessages(idRoom: string, participants: string[]) {
+        const data = { idRoom, participants };
+        console.log(data);
+        try {
+            return new Promise((resolve, reject) => {
+                this.http
+                    .post(`${this.localURL}/chat/getChatMessages/${idRoom}`, {
+                        participants,
+                    })
+                    .subscribe(
+                        (data: any) => {
+                            console.log(data);
+                            resolve(data);
+                        },
+                        (error) => {
+                            reject(error);
+                        }
+                    );
+            });
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async saveChatMessages(messages: MessageInterface) {
+        console.log(messages);
+        const { idRoom } = messages;
+        try {
+            return new Promise((resolve, reject) => {
+                this.http
+                    .post(`${this.localURL}/chat/saveChatMessages/${idRoom}`, messages)
+                    .subscribe(
+                        (data: any) => {
+                            console.log(data);
+                            resolve(data);
+                        },
+                        (error) => {
+                            console.log(error);
+                            reject(error);
+                        }
+                    );
+            });
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+}
+
