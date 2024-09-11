f58118920141abdb9595543add315e7ee0a909be
cesarp <capr99@hotmail.com>
2024-09-11 00:55:59 -0400

    boton burbuja de chatasda

--- src/app/pages/home/saved/saved.module.ts
+++ src/app/pages/home/saved/saved.module.ts
@@ -5,14 +5,17 @@ import { IonicModule } from '@ionic/angular';
 import { SavedPageRoutingModule } from './saved-routing.module';
 import { SavedPage } from './saved.page';
 import { StorageComponent } from 'src/app/components/storage/storage.component';
+import { DriverChipComponent } from 'src/app/components/driver-chip/driver-chip.component';
+import { SharedModule } from 'src/app/shared/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     SavedPageRoutingModule
   ],
-  declarations: [SavedPage, StorageComponent],
+  declarations: [SavedPage,/* DriverChipComponent, */StorageComponent],
 })
 export class SavedPageModule { }
